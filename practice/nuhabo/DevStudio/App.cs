66e585cc48c359d5f0dcfe5ae7d655516d471cb8
nuhabo <nuhabo03@gmail.com>
2021-09-11 13:14:45 +0900

    update

---
+++ practice/nuhabo/DevStudio/App.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace DevStudio
+{
+    public class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ practice/nuhabo/DevStudio/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevStudio
+{
+    public class Startup
+    {
+        [STAThread]
+        public static void Main(string [] args)
+        {
+            _ = new App().Run();
+            Environment.Exit(0);
+        }
+
+    }
+}
