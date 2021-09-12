4ef82b7ad28e688adc2469fdf777ac6c8fc5b3c2
nuhabo <nuhabo03@gmail.com>
2021-09-12 16:17:19 +0900

    update

---
+++ practice/nuhabo/Dev.Data/Menus/TreeMenuModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev.Data.Menus
+{
+    public class TreeMenuModel : MenuModel
+    {
+        public List<TreeMenuModel> ChildItems { get; set; }
+    }
+}
