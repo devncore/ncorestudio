1a97dcea5e2e4ccd933a3067088834cbeab0940c
rlagurdls456 <rlagurdls456@outlook.com>
2021-09-12 18:39:58 +0900

    Update

---
+++ src/Ncore.Controls/Utils/GeometryExtend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Ncore.Controls.Utils
+{
+	public static class GeometryExtend
+	{
+		#region ToGeometry
+
+		public static Geometry ToGeometry(this string geometryString)
+		{
+			return Geometry.Parse(geometryString);
+		}
+		#endregion
+	}
+}
