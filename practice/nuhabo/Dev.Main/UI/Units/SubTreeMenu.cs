37d5b8d94c59517eb0cafeb5cd0dde8927313903
nuhabo <nuhabo03@gmail.com>
2021-09-13 23:18:09 +0900

    update

--- practice/nuhabo/Dev.Main/UI/Units/SubTreeMenu.cs
+++ practice/nuhabo/Dev.Main/UI/Units/SubTreeMenu.cs
@@ -3,13 +3,20 @@ using System.Windows.Controls;
 
 namespace Dev.Main.UI.Units
 {
-    public class SubTreeMenu : Control
+    public class SubTreeMenu : TreeView
     {
         #region DefaultStyleKey
 
         static SubTreeMenu()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(SubTreeMenu), new FrameworkPropertyMetadata(typeof(SubTreeMenu)));
+        }
+        #endregion
+
+        #region GetContainerForItemOverride
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new SubTreeMenuItem();
         }
         #endregion
     }
--- practice/nuhabo/Dev.Main/UI/Units/SubTreeMenuItem.cs
+++ practice/nuhabo/Dev.Main/UI/Units/SubTreeMenuItem.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Dev.Main.UI.Units
 {
-    #region DefaultStyleKey
+    
 
-    public class SubTreeMenuItem : Control
+    public class SubTreeMenuItem : TreeViewItem
     {
+        #region DefaultStyleKey
+
         static SubTreeMenuItem()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(SubTreeMenuItem), new FrameworkPropertyMetadata(typeof(SubTreeMenuItem)));
         }
+
+        #endregion
+
+        #region GetContainerForItemOverride
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new SubTreeMenuItem();
+        }
+        #endregion
     }
-    #endregion
+
+
+
 }
