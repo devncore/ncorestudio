b2a3a7108c282a430d3aaae3bc4dc21dfd609a43
unknown <lucas.park@devncore.org>
2021-09-18 14:59:29 +0900

    update

--- practice/dev/DevStudio/App.cs
+++ practice/dev/DevStudio/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dev.Main.UI.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ namespace DevStudio
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            
+            _ = new MainWindow().ShowDialog();
             Environment.Exit(0);
         }
 
